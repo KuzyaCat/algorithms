d172c4081371a2b4471845718496e2a79cba2a89
KuzyaCat <lizerd34@mail.ru>
2020-12-18 11:37:46 +0300

    Add big graph example

--- minimum-spanning-tree/minimum-spanning-tree/Program.cs
+++ minimum-spanning-tree/minimum-spanning-tree/Program.cs
@@ -8,14 +8,15 @@ namespace minimum_spanning_tree
     {
         static void Main(string[] args)
         {
-            Kruskal kruskal = new Kruskal(GraphExamples.GRAPH_WEIGHT_MATRIX_SMALL);
+            // Kruskal kruskal = new Kruskal(GraphExamples.GRAPH_WEIGHT_MATRIX_SMALL);
+            Kruskal kruskal = new Kruskal(GraphExamples.GRAPH_WEIGHT_MATRIX_BIG);
             Console.WriteLine("kruskal:");
-            Console.WriteLine("kruskal.GetMinimumSpanningTree() " + kruskal.GetMinimumSpanningTree().Count);
             PrintEdges.PrintEdgesList(kruskal.GetMinimumSpanningTree());
             
             // Prim prim = new Prim(GraphExamples.GRAPH_WEIGHT_MATRIX_SMALL);
-            // Console.WriteLine("Prim:");
-            // PrintEdges.PrintEdgesList(kruskal.GetMinimumSpanningTree());
+            Prim prim = new Prim(GraphExamples.GRAPH_WEIGHT_MATRIX_BIG);
+            Console.WriteLine("Prim:");
+            PrintEdges.PrintEdgesList(kruskal.GetMinimumSpanningTree());
         }
     }
 }
